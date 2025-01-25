e4b130a921ebcd0725fe44956d50551a0c9160ea
Nil <nil@MacBook-Pro-de-Nil.local>
2025-01-26 00:10:18 +0900

    like?

--- routes/likeRoutes.js
+++ routes/likeRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 const { toggleLike, getLikedSakes, checkLikeStatus } = require('../controllers/likeController');
 
-router.post('/api/likes/toggle', protect, toggleLike);
-router.get('/api/likes', protect, getLikedSakes);
-router.get('/api/likes/check/:sakeId', protect, checkLikeStatus);
+router.post('/toggle', protect, toggleLike);
+router.get('/', protect, getLikedSakes);
+router.get('/check/:sakeId', protect, checkLikeStatus);
 
 module.exports = router;
