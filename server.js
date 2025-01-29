52176b847c44be33edc862936f0c88c31051940f
Nil <nil@MacBook-Pro-de-Nil.local>
2025-01-29 13:15:32 +0900

    like?

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const app = express();
 
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://sakemikke.vercel.app'],
+  origin: '*',
+ // origin: ['http://localhost:3000', 'https://sakemikke.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
